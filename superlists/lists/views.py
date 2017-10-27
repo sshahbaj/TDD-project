75c204bb819211a6738ab3022f13992bdf3bb729
sshahbaj <shahbaj007@gmail.com>
2017-10-28 02:50:32 +0530

    Add URL for creating new list via POST

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -4,11 +4,11 @@ from .models import Item
 
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
-
-    items = Item.objects.all()
+    # if request.method == 'POST':
+    #     Item.objects.create(text=request.POST['item_text'])
+    #     return redirect('/lists/the-only-list-in-the-world/')
+    #
+    # items = Item.objects.all()
     return render(request, 'lists/home.html')
 
 
@@ -17,3 +17,8 @@ def view_list(request):
     return render(request, 'lists/list.html', {
         'items': items
     })
+
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
